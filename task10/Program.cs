91d4ff7ea12169b3046042d7ff8ad4087f473cd8
Renata19 <renatalsa@mail.ru>
2022-09-10 00:14:13 +0700

    10

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число от 100 до 999:");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int digit = number / 10;
+    int count = digit % 10; 
+
+    Console.WriteLine($"Второе число {count}");
+
