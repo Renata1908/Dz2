db2fd29e8f9c41522b315600ded636e072aa566f
Renata19 <renatalsa@mail.ru>
2022-09-10 21:44:57 +0700

    15

---
+++ task15/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 1) 
+{
+Console.WriteLine("нет ");
+}
+else if(number == 2) 
+{
+Console.WriteLine("нет ");
+}
+else if(number == 3) 
+{
+Console.WriteLine("нет ");
+}
+else if(number == 4) 
+{
+Console.WriteLine("нет ");
+}
+else if(number == 5) 
+{
+Console.WriteLine("нет ");
+}
+else if(number == 6) 
+{
+Console.WriteLine("да");
+}
+else if(number == 7) 
+{
+Console.WriteLine("да");
+}
+else 
+{
+ Console.WriteLine("Некорректное число ");
+}
