161e1c8d7e7b9a9c91d2e39294b69caf38322326
Renata19 <renatalsa@mail.ru>
2022-09-13 15:33:59 +0700

    13

---
+++ task 13/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Введите число: ");
+long number = Convert.ToInt64(Console.ReadLine());
+
+if (Math.Abs(number) > 99)
+{
+while (Math.Abs(number) > 999)
+{
+number=number/10;
+}
+long digit3=number % 10;
+System.Console.WriteLine(Math.Abs(digit3));
+}
+else
+
+System.Console.WriteLine("Нет третьего числа");
+
